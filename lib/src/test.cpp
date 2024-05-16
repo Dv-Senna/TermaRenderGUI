2a8389849f6ee929fd21aa0d1eb10ee8bc19487b
Dv-Senna <dvsenna64@gmail.com>
2024-05-16 22:03:27 +0200

    Insert template

---
+++ lib/src/test.cpp
@@ -0,0 +1,16 @@
+#include "test.hpp"
+
+#include <iostream>
+
+
+namespace test {
+	void sayHello() {
+		#ifdef TR_WINDOWS
+			std::cout << "Hello from Windows TermRenderGUI !" << std::endl;
+		#elifdef TR_LINUX
+			std::cout << "Hello from Linux TermRenderGUI !" << std::endl;
+		#elifdef TR_APPLE
+			std::cout << "Hello from Apple TermRenderGUI !" << std::endl;
+		#endif
+	}
+}
